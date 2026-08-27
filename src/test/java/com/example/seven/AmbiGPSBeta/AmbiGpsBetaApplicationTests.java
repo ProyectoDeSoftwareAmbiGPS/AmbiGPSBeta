de4e1b27a618b4dc736ee304585e65386e2544e6
Alexsjka <alejandromino903@gmail.com>
2026-08-26 22:16:41 -0500

    ProyectoAmbiGPSBeta

---
+++ src/test/java/com/example/seven/AmbiGPSBeta/AmbiGpsBetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seven.AmbiGPSBeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmbiGpsBetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
